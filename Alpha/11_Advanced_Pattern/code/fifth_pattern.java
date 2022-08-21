1150802139e39acf33c60891d2d8400d72fba5c2
Aayush <aayushp454@gmail.com>
2022-08-21 21:59:00 +0530

    change in 0-1 pattern another method to solve

--- Alpha/11_Advanced_Pattern/code/fifth_pattern.java
+++ Alpha/11_Advanced_Pattern/code/fifth_pattern.java
@@ -1,33 +1,49 @@
 // ===> 0-1 Triangle pattern.
 
 public class fifth_pattern {
-    public static void zero_one_Triangle(int n) {
-        // outer loop ---> for rows.
-        for (int i = 1; i <= n; i++) {
-            // i ---> row number 
-            // j ---> cursor pointing on row
+    // ---> My method
+    // public static void zero_one_Triangle(int n) {
+    //     // outer loop ---> for rows.
+    //     for (int i = 1; i <= n; i++) {
+    //         // i ---> row number 
+    //         // j ---> cursor pointing on row
 
-            // inner loop ---> print 0 or 1.
+    //         // inner loop ---> print 0 or 1.
+    //         for (int j = 1; j <= i; j++) {
+    //             // if row number is odd... 
+    //             if(i%2 == 1) {
+    //                 // if cursor number is odd then print 1 otherwise 0.
+    //                 if(j%2 == 1) {
+    //                     System.out.print(1+" ");
+    //                 }
+    //                 else {
+    //                     System.out.print(0+" ");
+    //                 }
+    //             }
+    //             // if row number is even then...
+    //             else {
+    //                 // if cursor number is odd then print 0 otherwise 1.
+    //                 if(j%2 == 1) {
+    //                     System.out.print(0+" ");
+    //                 }
+    //                 else {
+    //                     System.out.print(1+" ");
+    //                 }
+    //             }
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    // Apna college method.
+    public static void zero_one_Triangle(int n) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                // if row number is odd... 
-                if(i%2 == 1) {
-                    // if cursor number is odd then print 1 otherwise 0.
-                    if(j%2 == 1) {
-                        System.out.print(1+" ");
-                    }
-                    else {
-                        System.out.print(0+" ");
-                    }
+                if((i+j)%2 == 0) {
+                    System.out.print(1+" ");
                 }
-                // if row number is even then...
                 else {
-                    // if cursor number is odd then print 0 otherwise 1.
-                    if(j%2 == 1) {
-                        System.out.print(0+" ");
-                    }
-                    else {
-                        System.out.print(1+" ");
-                    }
+                    System.out.print(0+" ");
                 }
             }
             System.out.println();
