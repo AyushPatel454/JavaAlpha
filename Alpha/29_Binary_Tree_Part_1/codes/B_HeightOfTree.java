be555a7241dc650ef5b9c2f802143374ed7b3e2f
Aayush Patel <aayushpatel25190@gmail.com>
2023-05-11 05:32:30 +0530

    Adding 29_Binary_Tree_Part_1 all codes.

---
+++ Alpha/29_Binary_Tree_Part_1/codes/B_HeightOfTree.java
@@ -0,0 +1,53 @@
+/*
+ * ===> Height of Tree.
+ * Find out height of Tree.
+ * 
+ * Time Complexity: O(n)
+ * 
+ * Using BFS we will find out.
+ */
+
+public class B_HeightOfTree {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null; 
+            this.right = null;
+        }
+    }
+
+    // Height of Tree.
+    public static int height(Node root) {
+        if(root == null) { // If Tree is empty then height = 0
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static void main(String[] args) {
+        /*
+         *          1
+         *         / \
+         *        2   3
+         *      / \  / \
+         *     4  5 6  7
+         */
+
+         Node root = new Node(1);
+         root.left = new Node(2);
+         root.right = new Node(3);
+         root.left.left = new Node(4);
+         root.left.right = new Node(5);
+         root.right.left = new Node(6);
+         root.right.right = new Node(7);
+
+         System.out.println("Height = "+height(root));
+    }
+}
